8f7cad03fb6d72f93bfb6545c5637494d3e738a5
Andy <g_andy@live.com>
2022-11-01 21:20:48 -0700

    Create simplewin.ex

---
+++ Examples/simplewin.ex
@@ -0,0 +1,40 @@
+--Simple Window
+--SDL2 Wrapper for Euphoria
+
+include std/ffi.e
+include sdl.e
+--include SDL_pixels.e
+
+constant MAX_WIDTH = 640,
+		 MAX_HEIGHT = 480
+		 
+public constant TRUE = 1,
+				FALSE = 0
+
+procedure main()
+
+ atom win = 0
+ object surf = {}
+ object event = allocate_struct(event)
+ atom quit = FALSE
+ 
+ if SDL_Init(SDL_INIT_VIDEO) = -1 then
+ 	puts(1,"Failed to init SDL!\n")
+ 	abort(0)
+ end if
+ 
+ win = SDL_CreateWindow("Simple Window",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,MAX_WIDTH,MAX_HEIGHT,SDL_WINDOW_SHOWN)
+ 
+ if win = -1 then
+ 	puts(1,"Failed to create window!\n")
+ 	abort(0)
+ end if
+ 
+ SDL_Delay(3000) --3 seconds
+ 
+ 
+ SDL_DestroyWindow(win)
+ SDL_Quit()
+end procedure
+
+main()
